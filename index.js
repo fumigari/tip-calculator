79c30a28dfc8e4cb5166d07da21c149c53b0b8da
Ariany Ferreira <arianysferreira1@gmail.com>
2023-09-16 23:05:54 -0300

    feat: add buttons listeners

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 //selectors
-
+const buttons = document.querySelectorAll(".percentage");
 //event listeners
+
+buttons.forEach(button => {
+	button.addEventListener("click", event => {
+		document.querySelector(".percentage.active")?.classList.remove("active");
+		event.currentTarget.classList.add("active");
+	})
+})
 
 //function
 
